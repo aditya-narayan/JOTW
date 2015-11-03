75db94b6b0e3bf22a6b3574f263a80e594b74b3d
aditya narayan <aditya.aaidu@gmail.com>
2015-11-03 15:29:22 +0530

    Initiate

---
+++ app/controllers/UserController.php
@@ -0,0 +1,222 @@
+<?php
+ 
+use Phalcon\Mvc\Model\Criteria;
+use Phalcon\Paginator\Adapter\Model as Paginator;
+
+class UserController extends ControllerBase
+{
+
+    /**
+     * Index action
+     */
+    public function indexAction()
+    {
+        $this->persistent->parameters = null;
+    }
+
+    /**
+     * Searches for user
+     */
+    public function searchAction()
+    {
+
+        $numberPage = 1;
+        if ($this->request->isPost()) {
+            $query = Criteria::fromInput($this->di, "User", $_POST);
+            $this->persistent->parameters = $query->getParams();
+        } else {
+            $numberPage = $this->request->getQuery("page", "int");
+        }
+
+        $parameters = $this->persistent->parameters;
+        if (!is_array($parameters)) {
+            $parameters = array();
+        }
+        $parameters["order"] = "id_user";
+
+        $user = User::find($parameters);
+        if (count($user) == 0) {
+            $this->flash->notice("The search did not find any user");
+
+            return $this->dispatcher->forward(array(
+                "controller" => "user",
+                "action" => "index"
+            ));
+        }
+
+        $paginator = new Paginator(array(
+            "data" => $user,
+            "limit"=> 10,
+            "page" => $numberPage
+        ));
+
+        $this->view->page = $paginator->getPaginate();
+    }
+
+    /**
+     * Displays the creation form
+     */
+    public function newAction()
+    {
+
+    }
+
+    /**
+     * Edits a user
+     *
+     * @param string $id_user
+     */
+    public function editAction($id_user)
+    {
+
+        if (!$this->request->isPost()) {
+
+            $user = User::findFirstByid_user($id_user);
+            if (!$user) {
+                $this->flash->error("user was not found");
+
+                return $this->dispatcher->forward(array(
+                    "controller" => "user",
+                    "action" => "index"
+                ));
+            }
+
+            $this->view->id_user = $user->id_user;
+
+            $this->tag->setDefault("id_user", $user->id_user);
+            $this->tag->setDefault("name", $user->name);
+            $this->tag->setDefault("email", $user->email);
+            
+        }
+    }
+
+    /**
+     * Creates a new user
+     */
+    public function createAction()
+    {
+
+        if (!$this->request->isPost()) {
+            return $this->dispatcher->forward(array(
+                "controller" => "user",
+                "action" => "index"
+            ));
+        }
+
+        $user = new User();
+
+        $user->name = $this->request->getPost("name");
+        $user->email = $this->request->getPost("email", "email");
+        
+
+        if (!$user->save()) {
+            foreach ($user->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+
+            return $this->dispatcher->forward(array(
+                "controller" => "user",
+                "action" => "new"
+            ));
+        }
+
+        $this->flash->success("user was created successfully");
+
+        return $this->dispatcher->forward(array(
+            "controller" => "user",
+            "action" => "index"
+        ));
+
+    }
+
+    /**
+     * Saves a user edited
+     *
+     */
+    public function saveAction()
+    {
+
+        if (!$this->request->isPost()) {
+            return $this->dispatcher->forward(array(
+                "controller" => "user",
+                "action" => "index"
+            ));
+        }
+
+        $id_user = $this->request->getPost("id_user");
+
+        $user = User::findFirstByid_user($id_user);
+        if (!$user) {
+            $this->flash->error("user does not exist " . $id_user);
+
+            return $this->dispatcher->forward(array(
+                "controller" => "user",
+                "action" => "index"
+            ));
+        }
+
+        $user->name = $this->request->getPost("name");
+        $user->email = $this->request->getPost("email", "email");
+        
+
+        if (!$user->save()) {
+
+            foreach ($user->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+
+            return $this->dispatcher->forward(array(
+                "controller" => "user",
+                "action" => "edit",
+                "params" => array($user->id_user)
+            ));
+        }
+
+        $this->flash->success("user was updated successfully");
+
+        return $this->dispatcher->forward(array(
+            "controller" => "user",
+            "action" => "index"
+        ));
+
+    }
+
+    /**
+     * Deletes a user
+     *
+     * @param string $id_user
+     */
+    public function deleteAction($id_user)
+    {
+
+        $user = User::findFirstByid_user($id_user);
+        if (!$user) {
+            $this->flash->error("user was not found");
+
+            return $this->dispatcher->forward(array(
+                "controller" => "user",
+                "action" => "index"
+            ));
+        }
+
+        if (!$user->delete()) {
+
+            foreach ($user->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+
+            return $this->dispatcher->forward(array(
+                "controller" => "user",
+                "action" => "search"
+            ));
+        }
+
+        $this->flash->success("user was deleted successfully");
+
+        return $this->dispatcher->forward(array(
+            "controller" => "user",
+            "action" => "index"
+        ));
+    }
+
+}
---
+++ app/models/User.php
@@ -0,0 +1,81 @@
+<?php
+
+use Phalcon\Mvc\Model\Validator\Email as Email;
+
+class User extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $id_user;
+
+    /**
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     *
+     * @var string
+     */
+    public $email;
+
+    /**
+     * Validations and business logic
+     *
+     * @return boolean
+     */
+    public function validation()
+    {
+        $this->validate(
+            new Email(
+                array(
+                    'field'    => 'email',
+                    'required' => true,
+                )
+            )
+        );
+
+        if ($this->validationHasFailed() == true) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'user';
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return User[]
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return User
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+}
